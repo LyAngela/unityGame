fe42260a6a265027cbd1e3679fc75ce048ae57f9
valeria <valeria.roblesgarzon@gmail.com>
2023-09-13 15:10:32 +0200

    Jump with spacebar and smooth camera

--- parkourGame/Assets/Scripts/Avatar.cs
+++ parkourGame/Assets/Scripts/Avatar.cs
@@ -9,10 +9,15 @@ public class Avatar : MonoBehaviour
     [SerializeField] private float _rotationSpeed = 90f; // Rotation speed in degrees per second
 
     private Rigidbody _rigidbody;
+    private bool _toJump = false;
+    private PhysicMaterial _bounciness;
+    private bool _isHighJumping = false;
+    
 
     private void Start()
     {
         _rigidbody = GetComponent<Rigidbody>();
+        _bounciness = GetComponent<Collider>().material;
     }
 
     private void OnMove(InputValue inputValue)
@@ -20,9 +25,34 @@ public class Avatar : MonoBehaviour
         _direction = inputValue.Get<Vector2>();
     }
     
-
+    private void OnJump()
+    {
+        _toJump = true;
+    }
+    private void OnCollisionEnter(Collision other) {
+        
+        if (other.collider.CompareTag("Floor")) {
+            _bounciness.bounceCombine = PhysicMaterialCombine.Maximum;
+            _isHighJumping = false;
+        }
+        
+    }
     private void FixedUpdate()
     {
+        if (_toJump)
+        {
+            _rigidbody.AddForce(Vector3.up * 10f, ForceMode.Impulse);
+            _isHighJumping = true;
+            _toJump = false;
+        }
+
+        if (_isHighJumping)
+        {
+            _bounciness.bounceCombine = PhysicMaterialCombine.Average;
+        }
+        
+     
+ 
         // Calculate movement vector
         Vector3 movement = new Vector3(_direction.x, 0, _direction.y) * _speed * Time.fixedDeltaTime;
 
@@ -34,4 +64,8 @@ public class Avatar : MonoBehaviour
         transform.Rotate(Vector3.up, rotationAmount);
       
     }
+
+   
+    
+   
 }
