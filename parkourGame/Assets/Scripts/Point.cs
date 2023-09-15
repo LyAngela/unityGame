3a6b8dc36df06404743d46ab52a116899cb93027
valeria <valeria.roblesgarzon@gmail.com>
2023-09-15 10:35:04 +0200

    Merge and sound of points work

--- parkourGame/Assets/Scripts/Point.cs
+++ parkourGame/Assets/Scripts/Point.cs
@@ -8,21 +8,22 @@ public class Point : MonoBehaviour
     [SerializeField] private AudioClip pointSound;
 
     private AudioSource _pointSource;
-
-    void Start()
-    {
-        _pointSource = gameObject.AddComponent<AudioSource>();
-        _pointSource.clip = pointSound;
-        _pointSource.volume = 0.5f;
-
-    }
+    
     public event EventHandler PointEvent;
     private void OnTriggerEnter(Collider other) {
         if (other.CompareTag("Player"))
         {
+            var go = new GameObject();
+            _pointSource = go.AddComponent<AudioSource>();
+            _pointSource.clip = pointSound;
+            _pointSource.volume = 0.5f;
             _pointSource.Play();
+            
+            
             PointEvent?.Invoke(this, EventArgs.Empty);
             Destroy(this.gameObject);
+            
+            Destroy(go, 2);
         }
     }
 }
