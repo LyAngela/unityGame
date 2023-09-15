9c8c3db1e8df830e27bd5cd9744377debfcc5310
valeria <valeria.roblesgarzon@gmail.com>
2023-09-15 16:03:51 +0200

    Added Missing Points and Count Down

--- parkourGame/Assets/Scripts/PointCounter.cs
+++ parkourGame/Assets/Scripts/PointCounter.cs
@@ -1,26 +1,47 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PointCounter : MonoBehaviour
 {
-    private int points = 0;
+    private int _points = 0;
+    private int _missingPoints;
+    private GUIStyle _style = new GUIStyle();
+    private float _countDown = 10;
     void Start()
     {
         useGUILayout = true;
-        Point[] pointsList = FindObjectsOfType<Point>();
+        _style.fontSize = 20;
+        
+        Point[] pointsList = GetComponentsInChildren<Point>();
+        _missingPoints = pointsList.Length;
+        
         foreach (var point in pointsList)
         {
-            point.PointEvent += (_, _) => points++;
-        } 
-           
-    }
+            point.PointEvent += (_, _) =>
+            {
+                _points++;
+                _missingPoints--;
+            };
+        }
+        
+        
     
+    }
+
+    private void Update()
+    {
+        _countDown = _countDown > 0 ? _countDown - Time.deltaTime : 0; 
+    }
+
     private void OnGUI()
     {
-    
-        GUILayout.BeginArea(new Rect(0,50, 300,300));
-        GUILayout.Label($"Points: {points}");
+        GUILayout.BeginArea(new Rect(0,20, 300,300));
+        GUILayout.Label($"Points: {_points}/{_missingPoints} ", _style);
+        GUILayout.Label($"Count Down: {(int)_countDown}",_style);
         GUILayout.EndArea();
     }
+    
+    
 }
